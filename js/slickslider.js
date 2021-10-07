5ae845bd8ca183917da8d6e3c33ae60caaf85152
Tanls86 <tanlspd04030@fpt.edu.vn>
2021-10-07 16:27:14 +0700

    add news

--- js/slickslider.js
+++ js/slickslider.js
@@ -24,20 +24,13 @@ $(document).ready(function () {
       }
   },
   {
-      breakpoint: 600,
-      settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-      }
-  },
-  {
-      breakpoint: 480,
+      breakpoint: 768,
       settings: {
           slidesToShow: 1,
           slidesToScroll: 1
       }
-  }
-  
+  },
+
   ]
       });
       $('.video_slider').slick({
